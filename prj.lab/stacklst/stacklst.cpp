ea90703439d42cb470562fe81697d2a0de74984f
MICRONAME <vanekpsevdonim5@gmail.com>
2024-02-19 13:11:45 +0300

    commit

--- prj.lab/stacklst/stacklst.cpp
+++ prj.lab/stacklst/stacklst.cpp
@@ -23,11 +23,25 @@ Stacklst &Stacklst::operator=(const Stacklst &rhs) {
   // дописать оператор= при разной длине стеков
   if (this != &rhs){
     if (rhs.head){
-      auto *tail = head = new Node(*rhs.head); // this is making a pair pointer to new node within rhs.head value
-      for (auto* p = rhs.head->prev; p; p = p->prev) // as pointer here is p of type Node, until p is nullptr
-        tail = tail->prev = new Node(*p); // makes tail pointer value and tail->next pointer valur as p
-
-      tail->prev = nullptr; //fixing making last tail->next pointer being Node
+      Node* tail;
+      if (head->prev != nullptr)
+        *tail = *head = Node(*rhs.head);
+      else
+        tail = head = new Node(*rhs.head);
+      //auto *tail = head = new Node(*rhs.head);
+      for (auto* p = rhs.head->prev; p; p = p->prev) {
+        if (tail->prev != nullptr)
+          *tail = *tail->prev = Node(*p);
+        else
+          tail = tail->prev = new Node(*p);
+      }
+      while (tail->prev != nullptr){
+        Node* temp;
+        temp = tail;
+        tail = tail->prev;
+        delete temp;
+      }
+      //tail->prev = nullptr; //fixing making last tail->next pointer being Node
     }
   }
   return *this;
--- prj.test/stackarr_test.cpp
+++ prj.test/stackarr_test.cpp
@@ -1,74 +1,40 @@
+//
+// Created by Егор Федюнин on 12.02.2024.
+//
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <Stackarr/Stackarr.hpp>
 #include "doctest.h"
-#include <iostream>
 
-TEST_CASE("cons") {
-SUBCASE("default") {
-Stackarr a;
-CHECK(a.size() == 0);
-}
-SUBCASE("copy") {
-Stackarr a;
-for (int i = 0; i < 5; ++i)
-a.push(Complex());
 
-Stackarr b(a);
-CHECK(b.size() == 5);
+#include <Stackarr/Stackarr.hpp>
 
-b.push(Complex());
-CHECK(b.size() == 6);
-CHECK(a.size() == 5);
-}
-}
-
-TEST_CASE("appr") {
-SUBCASE("appr with empty") {
-Stackarr a;
-Stackarr b;
-for (int i = 0; i < 5; ++i)
-b.push(Complex());
-a = b;
-CHECK(a.size() == 5);
-
-b.push(Complex());
-CHECK(b.size() == 6);
-CHECK(a.size() == 5);
-}
-SUBCASE("appr with full") {
-Stackarr a;
-Stackarr b;
-for (int i = 0; i < 5; ++i)
-{
-a.push(Complex());
-b.push(Complex());
-}
-a = b;
-CHECK(a.size() == 5);
-
-b.push(Complex());
-CHECK(b.size() == 6);
-CHECK(a.size() == 5);
-}
-}
-
-TEST_CASE("push, pop & top"){
-  Stackarr a;
-  CHECK(a.size() == 0);
-  for (int i = 0; i < 10; ++i)
-  {
-    a.push(Complex(i, i));
-    CHECK(a.size() == i + 1);
-    CHECK(a.top() == Complex(i, i));
+TEST_CASE("Testing StackArr class") {
+  SUBCASE("Testing methods"){
+    Stackarr arr;
+    Complex val = {1,0};
+    Complex val1 = {3,4};
+    arr.push(val);
+    CHECK(arr.top() == val);
+    arr.push(val1);
+    CHECK(arr.top() == val1);
+    arr.pop();
+    CHECK(arr.top() == val);
+    arr.pop();
+    CHECK(arr.empty());
+    arr.push(val);
+    arr.push(val1);
+    Stackarr arr1{arr};
+    CHECK(arr1.top() == val1);
+    arr.pop();
+    CHECK(arr.top() == val);
+    CHECK(arr1.top() == val1);
+    arr.clear();
+    arr.push(val);
+    arr.push(val1);
+    Stackarr arr2 = arr;
+    CHECK(arr2.top() == val1);
+    arr.pop();
+    CHECK(arr2.top() == val1);
+    CHECK(arr.top() == val);
   }
-
-  for (int i = 8; i >= 0; i--)
-  {
-    a.pop();
-    CHECK(a.size() == i + 1);
-    CHECK(a.top() == Complex(i, i));
-  }
-    a.pop();
-    CHECK_THROWS(a.top());
-    a.pop();
 }
